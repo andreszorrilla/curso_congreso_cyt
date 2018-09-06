75d1ad4f4079cef13dae996adb72d5cca66080da
Andres Zorrilla <azorrilla@uni.edu.py>
2018-09-06 13:27:23 -0400

    primer commit: agregando codigo

---
+++ aplicacion_curso_cyt/aplicacion_curso_cyt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for aplicacion_curso_cyt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aplicacion_curso_cyt.settings")
+
+application = get_wsgi_application()
